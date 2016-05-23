a772bd5b2b845dc2e93987b26460b126a1993a97
Oliver Lai <oliverlai55@gmail.com>
2016-05-22 20:22:52 -0400

    changing dropdown menu

--- dropdown/src/button.jsx
+++ dropdown/src/button.jsx
@@ -3,17 +3,20 @@
 
 var React = require('react');
 module.exports = React.createClass({
-  handleClick: function(){
-    alert('button clicked');
-  },
+  // handleClick: function(){
+  //   alert('button clicked');
+  // },
   render: function(){
 
     //make sure to use className
     return (
       //runs function handleClick()
-      <button onClick={this.handleClick} className={"btn " + this.props.className} type="button">
-    {this.props.title} <span className={this.props.subTitleClassName}> {this.props.subTitle} </span>
+      <button onClick={this.props.whenClicked} className={"btn " + this.props.className} type="button">
+    {this.props.title}
+    <span className={this.props.subTitleClassName}> {this.props.subTitle} </span>
     </button>
   );
   }
 });
+
+//pass it prop whenClicked reference to handleClick, which is in dropdown component
--- dropdown/src/dropdown.jsx
+++ dropdown/src/dropdown.jsx
@@ -4,14 +4,35 @@
 
 var React = require('react');
 var Button = require('./button');
-// var List = require('./list');
+var ListItem = require('./list-item');
 
 module.exports = React.createClass({
+  handleClick: function(){
+    alert('hello from dropdown');
+  },
+  getInitialState: function(){
+    return { open: false}
+    //lets dropdown NOT open initially
+  },
   render: function(){
+    var list = this.props.items.map(function(item){
+      return (
+        <ListItem item = {item} />
+      )
+    });
     return (
       <div className = "dropdown">
-        <Button className='btn-default' title={this.props.title} subTitleClassName= "caret" />
+      <Button
+      whenClicked = {this.handleClick}
+      className='btn-default'
+      title={this.props.title}
+      subTitleClassName= "caret"
+      />
+
+      <ul className={"dropdown-menu " + (this.state.open ? "show" : "") }>
+        {list}
+      </ul>
       </div>
-    );
+    )
   }
 });
